694b96c9dea11daa3b9560689f57ee41191141b9
HarshithaVegesna99 <76994452+HarshithaVegesna99@users.noreply.github.com>
2021-09-14 16:05:55 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,5 +1,11 @@
+def add(a, b):
+    return a + b
+
 def test_add():
     assert add(2, 3) == 5
-    
-def test_subtract():
+
+ def sub(a, b):
+  return a - b
+
+def test_sub():
     assert subtract(2, 3) == -1
